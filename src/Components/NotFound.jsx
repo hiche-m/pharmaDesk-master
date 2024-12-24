c22505000d4d0c0e4a38c5b478fb7b13666ca2e3
Mohamed Hichem Rahmani <hichem1029@live.fr>
2024-12-24 14:31:27 +0100

    fix: Changed the routing system

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -4,14 +4,9 @@ import { useNavigate } from "react-router-dom";
 const NotFound = () => {
     const navigate = useNavigate();
     return (<>
-        <div className="col-span-3 row-span-2 bg-lightShapes">
-
-        </div>
-        <div className="col-span-9 col-start-1 row-start-2 flex bg-background">
-            <div className="flex flex-col grow justify-center items-center">
-                <span className="text-xl text-textPrimary">Coming Soon...</span>
-                <span className="text-sm text-textSecoundary underline" onClick={() => navigate(-1)}>Go Back</span>
-            </div>
+        <div className="flex flex-col grow justify-center items-center">
+            <span className="text-xl text-textPrimary">Coming Soon...</span>
+            <span className="text-sm text-textSecoundary underline cursor-pointer" onClick={() => navigate(-1)}>Go Back</span>
         </div>
     </>
     );
