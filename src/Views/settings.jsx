c721dd52b224a38a1caf4e089749514d24b2596f
Mohamed Hichem Rahmani <hichem1029@live.fr>
2024-12-26 17:12:45 +0100

    Settings layout done; 1st section

--- src/Views/settings.jsx
+++ src/Views/settings.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import { FaPen } from "react-icons/fa";
 import { MdManageAccounts } from "react-icons/md";
 import { HiDotsHorizontal } from "react-icons/hi";
+import { FaTrashAlt } from "react-icons/fa";
+import { FaSave } from "react-icons/fa";
 import pfp4 from "../Assets/Images/pfp4.svg"
+import SettingsInput from "../Components/SettingsInput.jsx";
 
 const Settings = () => {
 
     const [selectedIndex, setSelectedIndex] = useState(0);
 
-    const settingsTiles = ["Compte Pharmacie", "Notifications", "Profiles"];
+    const settingsTiles = ["Compte Pharmacie", "Compte Personnel", "Sécurité", "Notifications", "Profiles"];
 
     const handleEditClick = () => {
         console.log('Edit');
@@ -17,7 +20,7 @@ const Settings = () => {
 
     const handleAccountClick = () => {
         console.log('Account');
-        setSelectedIndex(2);
+        setSelectedIndex(settingsTiles.indexOf('Profiles'));
     };
 
     const handleDropdownClick = () => {
@@ -32,15 +35,15 @@ const Settings = () => {
         {/* Profile */}
         <div className="col-span-12 row-span-3 ml-4 inline-flex flex-row justify-between items-center">
             <div className="inline-flex flex-row space-x-2 items-center">
-                <img className="h-12 w-12 rounded-full bg-gray-500" src={pfp4} />
+                <img className="hidden sm:block h-12 w-12 rounded-full bg-gray-500" src={pfp4} />
                 <span className="flex flex-col">
                     <span className="font-medium">Harrison Pfannerstill</span>
                     <span className="text-sm text-textSecoundary">Administrateur</span>
                 </span>
             </div>
             <div className="inline-flex flex-row space-x-1">
-                <span className="rounded-full cursor-pointer bg-primary p-3 hover:bg-primary/90 text-white active:bg-darkPrimary" onClick={() => handleEditClick()}><FaPen /></span>
-                <span className={`rounded-full ${selectedIndex === 2 ? '' : 'cursor-pointer'} ${selectedIndex === 2 ? 'bg-textSecoundary/70' : 'bg-disabled'} p-3 ${selectedIndex === 2 ? '' : 'hover:bg-disabled/90'} text-textPrimary active:bg-textSecoundary/70`} onClick={selectedIndex === 2 ? null : () => handleAccountClick()}><MdManageAccounts /></span>
+                <span className="hidden sm:block rounded-full cursor-pointer bg-primary p-3 hover:bg-primary/90 text-white active:bg-darkPrimary" onClick={() => handleEditClick()}><FaPen /></span>
+                <span className={`hidden sm:block rounded-full ${selectedIndex === settingsTiles.indexOf('Profiles') ? '' : 'cursor-pointer'} ${selectedIndex === settingsTiles.indexOf('Profiles') ? 'bg-textSecoundary/70' : 'bg-disabled'} p-3 ${selectedIndex === settingsTiles.indexOf('Profiles') ? '' : 'hover:bg-disabled/90'} text-textPrimary active:bg-textSecoundary/70`} onClick={selectedIndex === settingsTiles.indexOf('Profiles') ? null : () => handleAccountClick()}><MdManageAccounts /></span>
                 <span className="rounded-full cursor-pointer bg-disabled p-3 hover:bg-disabled/90 text-textPrimary active:bg-textSecoundary/70" onClick={() => handleDropdownClick()}><HiDotsHorizontal /></span>
             </div>
         </div>
@@ -51,25 +54,52 @@ const Settings = () => {
         {/* Content */}
         <div className="col-span-12 row-span-10 inline-flex flex-row bg-superClear rounded-xl shadow-md">
             {/* Side Menu */}
-            <div className="bg-lightShapes flex flex-col space-y-4 p-4 rounded-tl-xl">
+            <div className={`bg-lightShapes hidden small:flex flex-col space-y-4 p-4 rounded-tl-xl`}>
                 {settingsTiles.map((item, index) => (<span className={`font-medium cursor-pointer ${index === selectedIndex ? 'text-textPrimary' : 'text-textSecoundary'}`} key={`setting-item-tile-${index}-${item}`} onClick={() => handleMenuSwitch(index)}>{item}</span>))}
             </div>
 
             {/* Content */}
             <div className="w-full">
                 {/* Account Settings */}
-                <div className={`${selectedIndex === 0 ? 'flex' : 'hidden'}`}>
-                    {settingsTiles[0]}
+                <div className={`${selectedIndex === 0 ? 'flex' : 'hidden'} flex-col p-4 space-y-4`}>
+                    <span className="font-medium">Information sur la pharmacie</span>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 overflow-y-auto gap-x-2 small:gap-x-10">
+                        <SettingsInput placeholder='Nom du pharmacie...' flexible={true} />
+                        <SettingsInput placeholder='Adresse du pharmacie...' flexible={true} />
+                        <SettingsInput placeholder='Numéro téléphone pharmacie...' flexible={true} />
+                        <SettingsInput placeholder='Description...' large={true} flexible={true} />
+                        <SettingsInput placeholder='Latitude' flexible={true} />
+                        <SettingsInput placeholder='Longitude' flexible={true} />
+                    </div>
+                    <div className="inline-flex grow justify-between">
+                        <button className="text-textSecoundary">
+                            <span className="inline-flex flex-row space-x-2 text-base items-center underline italic">
+                                <FaTrashAlt />
+                                <span>Clear</span>
+                            </span>
+                        </button>
+                        <button className="bg-primary p-2 rounded-lg hover:bg-primary/90 active:bg-darkPrimary">
+                            <span className="inline-flex flex-row space-x-2 text-base items-center px-2 text-white">
+                                <FaSave />
+                                <span>Save</span>
+                            </span>
+                        </button>
+                    </div>
                 </div>
 
-                {/* Notification Settings */}
+                {/* Personal Info */}
                 <div className={`${selectedIndex === 1 ? 'flex' : 'hidden'}`}>
                     {settingsTiles[1]}
                 </div>
 
-                {/* Profiles Settings */}
+                {/* Notification Settings */}
                 <div className={`${selectedIndex === 2 ? 'flex' : 'hidden'}`}>
                     {settingsTiles[2]}
+                </div>
+
+                {/* Profiles Settings */}
+                <div className={`${selectedIndex === 3 ? 'flex' : 'hidden'}`}>
+                    {settingsTiles[3]}
                 </div>
             </div>
         </div>
