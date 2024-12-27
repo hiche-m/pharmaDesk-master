21e5989493a5aeff3eab4bb45dc4331dc9d6a675
Mohamed Hichem Rahmani <hichem1029@live.fr>
2024-12-27 13:07:23 +0100

    feat: Settings UI Complete

--- src/Components/ToggleSwitch.jsx
+++ src/Components/ToggleSwitch.jsx
@@ -4,13 +4,13 @@ const ToggleSwitch = ({ value, toggleSwitch, className }) => {
 
     return (
         <div
-            className={`relative inline-flex items-center h-6 rounded-full w-11 cursor-pointer transition-colors duration-300 ${value ? 'bg-primary' : 'bg-gray-400'
+            className={`relative inline-flex items-center min-h-6 max-h-6 rounded-full min-w-11 max-w-11 cursor-pointer transition-colors duration-300 ${value ? 'bg-primary' : 'bg-gray-400'
                 } ${className}`}
             onClick={() => toggleSwitch()}
         >
             <span
                 className={`transform transition-transform duration-300 ${value ? 'translate-x-6' : 'translate-x-1'
-                    } inline-block w-4 h-4 bg-white rounded-full`}
+                    } inline-block min-w-4 max-w-4 min-h-4 max-h-4 bg-white rounded-full`}
             ></span>
         </div>
     );
--- src/Views/settings.jsx
+++ src/Views/settings.jsx
@@ -4,14 +4,18 @@ import { MdManageAccounts } from "react-icons/md";
 import { HiDotsHorizontal } from "react-icons/hi";
 import { FaTrashAlt } from "react-icons/fa";
 import { FaSave } from "react-icons/fa";
+import { IoMdAdd } from "react-icons/io";
 import pfp4 from "../Assets/Images/pfp4.svg"
 import SettingsInput from "../Components/SettingsInput.jsx";
+import ToggleSwitch from "../Components/ToggleSwitch.jsx";
+import DropdownMenu from "../Components/DropDownMenu.jsx";
+import { toast } from "react-toastify";
 
 const Settings = () => {
 
     const [selectedIndex, setSelectedIndex] = useState(0);
 
-    const settingsTiles = ["Compte Pharmacie", "Compte Personnel", "Sécurité", "Notifications", "Profiles"];
+    const settingsTiles = ["Compte Pharmacie", "Compte Personnel", "Sécurité", "Notifications", "Profils"];
 
     const handleEditClick = () => {
         console.log('Edit');
@@ -20,7 +24,7 @@ const Settings = () => {
 
     const handleAccountClick = () => {
         console.log('Account');
-        setSelectedIndex(settingsTiles.indexOf('Profiles'));
+        setSelectedIndex(settingsTiles.indexOf('Profils'));
     };
 
     const handleDropdownClick = () => {
@@ -43,7 +47,7 @@ const Settings = () => {
             </div>
             <div className="inline-flex flex-row space-x-1">
                 <span className="hidden sm:block rounded-full cursor-pointer bg-primary p-3 hover:bg-primary/90 text-white active:bg-darkPrimary" onClick={() => handleEditClick()}><FaPen /></span>
-                <span className={`hidden sm:block rounded-full ${selectedIndex === settingsTiles.indexOf('Profiles') ? '' : 'cursor-pointer'} ${selectedIndex === settingsTiles.indexOf('Profiles') ? 'bg-textSecoundary/70' : 'bg-disabled'} p-3 ${selectedIndex === settingsTiles.indexOf('Profiles') ? '' : 'hover:bg-disabled/90'} text-textPrimary active:bg-textSecoundary/70`} onClick={selectedIndex === settingsTiles.indexOf('Profiles') ? null : () => handleAccountClick()}><MdManageAccounts /></span>
+                <span className={`hidden sm:block rounded-full ${selectedIndex === settingsTiles.indexOf('Profils') ? '' : 'cursor-pointer'} ${selectedIndex === settingsTiles.indexOf('Profils') ? 'bg-textSecoundary/70' : 'bg-disabled'} p-3 ${selectedIndex === settingsTiles.indexOf('Profils') ? '' : 'hover:bg-disabled/90'} text-textPrimary active:bg-textSecoundary/70`} onClick={selectedIndex === settingsTiles.indexOf('Profils') ? null : () => handleAccountClick()}><MdManageAccounts /></span>
                 <span className="rounded-full cursor-pointer bg-disabled p-3 hover:bg-disabled/90 text-textPrimary active:bg-textSecoundary/70" onClick={() => handleDropdownClick()}><HiDotsHorizontal /></span>
             </div>
         </div>
@@ -61,17 +65,19 @@ const Settings = () => {
             {/* Content */}
             <div className="w-full">
                 {/* Account Settings */}
-                <div className={`${selectedIndex === 0 ? 'flex' : 'hidden'} flex-col p-4 space-y-4`}>
-                    <span className="font-medium">Information sur la pharmacie</span>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 overflow-y-auto gap-x-2 small:gap-x-10">
-                        <SettingsInput placeholder='Nom du pharmacie...' flexible={true} />
-                        <SettingsInput placeholder='Adresse du pharmacie...' flexible={true} />
-                        <SettingsInput placeholder='Numéro téléphone pharmacie...' flexible={true} />
-                        <SettingsInput placeholder='Description...' large={true} flexible={true} />
-                        <SettingsInput placeholder='Latitude' flexible={true} />
-                        <SettingsInput placeholder='Longitude' flexible={true} />
+                <div className={`${selectedIndex === 0 ? 'flex' : 'hidden'} flex-col h-full justify-between p-4 space-y-4`}>
+                    <div className="flex flex-col space-y-2">
+                        <span className="font-medium">Informations sur la pharmacie</span>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 overflow-y-auto gap-x-2 small:gap-x-10">
+                            <SettingsInput placeholder='Nom du pharmacie...' flexible={true} />
+                            <SettingsInput placeholder='Adresse du pharmacie...' flexible={true} />
+                            <SettingsInput placeholder='Numéro téléphone pharmacie...' flexible={true} />
+                            <SettingsInput placeholder='Description...' large={true} flexible={true} />
+                            <SettingsInput placeholder='Latitude' flexible={true} />
+                            <SettingsInput placeholder='Longitude' flexible={true} />
+                        </div>
                     </div>
-                    <div className="inline-flex grow justify-between">
+                    <div className="inline-flex w-full justify-between">
                         <button className="text-textSecoundary">
                             <span className="inline-flex flex-row space-x-2 text-base items-center underline italic">
                                 <FaTrashAlt />
@@ -88,18 +94,147 @@ const Settings = () => {
                 </div>
 
                 {/* Personal Info */}
-                <div className={`${selectedIndex === 1 ? 'flex' : 'hidden'}`}>
-                    {settingsTiles[1]}
+                <div className={`${selectedIndex === 1 ? 'flex' : 'hidden'} flex-col h-full justify-between p-4 space-y-4`}>
+                    <div className="flex flex-col space-y-2">
+                        <span className="font-medium">Informations personnelles </span>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 overflow-y-auto gap-x-2 small:gap-x-10">
+                            <SettingsInput placeholder='Votre nom...' flexible={true} />
+                            <SettingsInput placeholder='Votre prénom...' flexible={true} />
+                            <SettingsInput placeholder='Votre numéro téléphone...' flexible={true} />
+                            <SettingsInput placeholder='Votre adresse complète...' large={true} flexible={true} />
+                        </div>
+                    </div>
+                    <div className="inline-flex w-full justify-between">
+                        <button className="text-textSecoundary">
+                            <span className="inline-flex flex-row space-x-2 text-base items-center underline italic">
+                                <FaTrashAlt />
+                                <span>Clear</span>
+                            </span>
+                        </button>
+                        <button className="bg-primary p-2 rounded-lg hover:bg-primary/90 active:bg-darkPrimary">
+                            <span className="inline-flex flex-row space-x-2 text-base items-center px-2 text-white">
+                                <FaSave />
+                                <span>Save</span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+
+                {/* Security */}
+                <div className={`${selectedIndex === 2 ? 'flex' : 'hidden'} flex-col h-full justify-between p-4 space-y-4`}>
+                    <div className="flex flex-col space-y-2">
+                        <span className="font-medium">Informations de sécurité</span>
+                        <div className="grid grid-cols-1 overflow-y-auto mr-2 small:mr-20">
+                            <SettingsInput placeholder='Adresse mail...' flexible={true} />
+                            <span className="col-span-1 h-5" />
+                            <SettingsInput placeholder='Nouveau mot de passe...' flexible={true} />
+                            <SettingsInput placeholder='Confirmer le nouveau mot de passe...' flexible={true} />
+                            <span className="col-span-1 h-5" />
+                            <SettingsInput placeholder='Votre mot de passe actuel...' flexible={true} />
+                        </div>
+                    </div>
+                    <div className="inline-flex w-full justify-between">
+                        <button className="text-textSecoundary">
+                            <span className="inline-flex flex-row space-x-2 text-base items-center underline italic">
+                                <FaTrashAlt />
+                                <span>Clear</span>
+                            </span>
+                        </button>
+                        <button className="bg-primary p-2 rounded-lg hover:bg-primary/90 active:bg-darkPrimary">
+                            <span className="inline-flex flex-row space-x-2 text-base items-center px-2 text-white">
+                                <FaSave />
+                                <span>Save</span>
+                            </span>
+                        </button>
+                    </div>
                 </div>
 
                 {/* Notification Settings */}
-                <div className={`${selectedIndex === 2 ? 'flex' : 'hidden'}`}>
-                    {settingsTiles[2]}
+                <div className={`${selectedIndex === 3 ? 'flex' : 'hidden'} flex-col h-full justify-between p-4 space-y-4`}>
+                    <div className="flex flex-col space-y-2">
+                        <span className="font-medium">Paramètres de notifications</span>
+                        <div className="grid grid-cols-1 overflow-y-auto space-y-2 py-2">
+                            {/* First Row */}
+                            <div className="inline-flex flex-row w-full justify-between">
+                                <span className="flex flex-col">
+                                    <span>Émettre un son lorsqu'une notification d'ordonnance arrive.</span>
+                                    <span className="text-textSecoundary italic text-sm">Nouvelle notification!</span>
+                                </span>
+                                <ToggleSwitch value={true} toggleSwitch={() => { }} />
+                            </div>
+                            {/* Secound Row */}
+                            <div className="inline-flex flex-row w-full justify-between">
+                                <span className="flex flex-col">
+                                    <span>Émettre un son lorsqu'un client confirme qu'il va venir.</span>
+                                    <span className="text-textSecoundary italic text-sm">Un client arrive!</span>
+                                </span>
+                                <ToggleSwitch value={false} toggleSwitch={() => { }} />
+                            </div>
+                            {/* Third Row */}
+                            <div className="inline-flex flex-row w-full justify-between">
+                                <span className="flex flex-col">
+                                    <span>Afficher une boîte de dialogue toast lorsque des événements se produisent.</span>
+                                    <span className="text-textSecoundary underline text-sm cursor-pointer" onClick={() => toast('Un dialogue toast!')}>Cliquez ici pour prévisualiser</span>
+                                </span>
+                                <ToggleSwitch value={true} toggleSwitch={() => { }} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="inline-flex w-full justify-between">
+                        <button className="text-textSecoundary">
+                            <span className="inline-flex flex-row space-x-2 text-base items-center underline italic">
+                                <FaTrashAlt />
+                                <span>Clear</span>
+                            </span>
+                        </button>
+                        <button className="bg-primary p-2 rounded-lg hover:bg-primary/90 active:bg-darkPrimary">
+                            <span className="inline-flex flex-row space-x-2 text-base items-center px-2 text-white">
+                                <FaSave />
+                                <span>Save</span>
+                            </span>
+                        </button>
+                    </div>
                 </div>
 
-                {/* Profiles Settings */}
-                <div className={`${selectedIndex === 3 ? 'flex' : 'hidden'}`}>
-                    {settingsTiles[3]}
+                {/* Profils Settings */}
+                <div className={`${selectedIndex === 4 ? 'flex' : 'hidden'} flex-col h-full justify-between p-4 space-y-4`}>
+                    <div className="flex flex-col space-y-2">
+                        <div className="inline-flex flex-row w-full justify-between items-center py-2">
+                            <span className="font-medium">Gestion de profils</span>
+                            <IoMdAdd className="text-textSecoundary cursor-pointer" onClick={() => {
+                                console.log('Add Profile');
+                            }} />
+                        </div>
+                        <div className="flex flex-col space-y-2 py-2">
+                            <div className="flex flex-col small:inline-flex small:flex-row w-full justify-between items-start small:items-center">
+                                <div className="inline-flex flex-row space-x-2 items-center">
+                                    <img className="hidden sm:block h-12 w-12 rounded-full bg-gray-500" src={pfp4} />
+                                    <span className="flex flex-col">
+                                        <span className="font-medium">Harrison Pfannerstill</span>
+                                        <span className="text-sm text-textSecoundary">Administrateur</span>
+                                    </span>
+                                </div>
+                                <div className="flex flex-col space-y-0 py-0 sm:space-y-2 sm:py-2 sm:inline-flex sm:flex-row space-x-4 items-center">
+                                    <button className="text-red-500 underline text-sm">Supprimer</button>
+                                    <DropdownMenu options={[{ label: 'Administrateur', value: 'admin' }, { label: 'Vendeur', value: 'vendeur' }]} label={'Role'} selectedValue={'admin'} onSelect={(value) => { }} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="inline-flex w-full justify-between">
+                        <button className="text-textSecoundary">
+                            <span className="inline-flex flex-row space-x-2 text-base items-center underline italic">
+                                <FaTrashAlt />
+                                <span>Clear</span>
+                            </span>
+                        </button>
+                        <button className="bg-primary p-2 rounded-lg hover:bg-primary/90 active:bg-darkPrimary">
+                            <span className="inline-flex flex-row space-x-2 text-base items-center px-2 text-white">
+                                <FaSave />
+                                <span>Save</span>
+                            </span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
