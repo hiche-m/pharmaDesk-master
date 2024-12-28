9942eb6889d89ef42e248d012f8ed679077f5900
Mohamed Hichem Rahmani <hichem1029@live.fr>
2024-12-28 14:35:25 +0100

    feat: Added some validators; Added dialogs

--- src/Components/DropDownMenu.jsx
+++ src/Components/DropDownMenu.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { AiFillCaretDown } from "react-icons/ai";
 
-const DropdownMenu = ({ options, label, selectedValue, onSelect, disabled = false }) => {
+const DropdownMenu = ({ options, label, selectedValue, onSelect, disabled = false, className = '' }) => {
     const [isOpen, setIsOpen] = useState(false);
     const buttonRef = useRef(null);
     const dropdownRef = useRef(null);
@@ -22,7 +22,7 @@ const DropdownMenu = ({ options, label, selectedValue, onSelect, disabled = fals
     }, []);
 
     return (
-        <div className="relative inline-block text-left">
+        <div className={`${className} relative inline-block text-left`}>
             {/* Button to trigger dropdown */}
             <button
                 ref={buttonRef}
@@ -30,7 +30,7 @@ const DropdownMenu = ({ options, label, selectedValue, onSelect, disabled = fals
                 className={`${disabled ? 'border-gray-500' : 'border-primary'} border-2 bg-superClear ${disabled ? 'text-textSecoundary' : 'text-textPrimary'} px-2 py-2 rounded-md inline-flex justify-around items-center focus:outline-none z-0`}
             >
                 <span className="px-2">
-                    {label}
+                    {!label ? selectedValue : selectedValue ? options.filter(option => option.value === selectedValue)[0].label : label}
                 </span>
                 <AiFillCaretDown className="text-primary" />
             </button>
@@ -39,7 +39,7 @@ const DropdownMenu = ({ options, label, selectedValue, onSelect, disabled = fals
             {isOpen && (
                 <div
                     ref={dropdownRef}
-                    className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg z-10"
+                    className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg z-50"
                 >
                     <ul>
                         {options.map((option) => (
--- src/Components/SettingsInput.jsx
+++ src/Components/SettingsInput.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const SettingsInput = ({ label, placeholder, onChange, flexible = false, initialValue = '', large = false }) => {
-    const [value, setValue] = useState(initialValue);
-    const handleOnChange = (value) => {
-        setValue(value);
+const SettingsInput = ({ label, placeholder, onChange, value = '', flexible = false, large = false, obscure = false }) => {
+
+    const handleOnChange = (val) => {
         if (onChange) {
-            onChange(value);
+            onChange(val);
         }
     }
+
     return (<div className="flex flex-col space-y-2 py-2">
         {label && (<span className="text- font-medium">{label}</span>)}
         {large ?
-            (<textarea className={`bg-lightShapes p-2 outline-none rounded-md ${flexible ? '' : 'w-40'} resize-none`} placeholder={placeholder} value={value} onChange={(event) => handleOnChange(event.target.value)} />)
-            : (<input className={`bg-lightShapes p-2 outline-none rounded-md ${flexible ? '' : 'w-40'}`} placeholder={placeholder} value={value} onChange={(event) => handleOnChange(event.target.value)} />)}
+            (<textarea className={`bg-lightShapes p-2 outline-none rounded-md ${flexible ? '' : 'w-40'} resize-none`} placeholder={placeholder} value={value ? value : ''} onChange={(event) => handleOnChange(event.target.value)} />)
+            : (<input className={`bg-lightShapes p-2 outline-none rounded-md ${flexible ? '' : 'w-40'}`} placeholder={placeholder} value={value ? value : ''} onChange={(event) => handleOnChange(event.target.value)} type={obscure ? 'password' : 'text'} />)}
     </div>);
 }
 
