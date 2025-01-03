caeaeebe02cb4b9c3e939a54fc5a7a75fc9cfaaf
Mohamed Hichem Rahmani <hichem1029@live.fr>
2025-01-03 18:43:34 +0100

    fix: Fixed DailyIncome component bug

--- src/Components/DailyIncome.jsx
+++ src/Components/DailyIncome.jsx
@@ -1,47 +1,62 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AiFillCaretDown } from "react-icons/ai";
-import { calcPercentage, drawCircle, drawLine, formatDateForSql, formatNumberWithComma, formattedPreviousDates, getFullMonthNameInFrench, sqlToFrenchDateDaily } from "../Utils/Functions.jsx";
+import { calcPercentage, drawCircle, drawLine, formatNumberWithComma, formattedPreviousDates, getFullMonthNameInFrench, getTotalSalesForDates, sqlToFrenchDateDaily } from "../Utils/Functions.jsx";
+import { useStateContext } from "../Context/ContextProvider.jsx";
 
-const DailyIncome = ({ className = "", inputValues = [0, 0, 0], dailyWidgetDate = formatDateForSql(new Date()), dotWidth = 5 }) => {
+const DailyIncome = ({ className = "", dotWidth = 5 }) => {
+
     const canvasRef = useRef(null);
 
-    const values = inputValues ? inputValues : [0, 0, 0]
+    const {
+        dailyWidgetData, dailyWidgetDate
+    } = useStateContext();
+
+    const [result, setResult] = useState([0, 0, 0]);
 
     useEffect(() => {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
+        if (dailyWidgetData) {
+            const values = getTotalSalesForDates(dailyWidgetData, dailyWidgetDate)
 
-        // Set canvas dimensions based on its container
-        const { width, height } = canvas.getBoundingClientRect();
-        canvas.width = width;
-        canvas.height = height;
+            console.log(dailyWidgetData);
+            console.log(values);
 
-        const third = width / 3;
+            setResult(values);
 
-        // Example drawing on the canvas
-        ctx.fillStyle = 'rgba(255, 255, 255, 0)';
-        ctx.fillRect(0, 0, width, height);
-        ctx.beginPath();
+            const canvas = canvasRef.current;
+            const ctx = canvas.getContext('2d');
 
-        drawLine(ctx, third - third / 2,
-            height * (1 - calcPercentage(values[1], values[0])),
-            2 * third - third / 2,
-            height * 0.5);
-        drawLine(ctx, 2 * third - third / 2,
-            height * 0.5,
-            width - third / 2,
-            height * (1 - calcPercentage(values[1], values[2])));
+            // Set canvas dimensions based on its container
+            const { width, height } = canvas.getBoundingClientRect();
+            canvas.width = width;
+            canvas.height = height;
 
-        drawCircle(ctx, third - third / 2,
-            height * (1 - calcPercentage(values[1], values[0])),
-            dotWidth);
-        drawCircle(ctx, 2 * third - third / 2,
-            height * 0.5,
-            dotWidth);
-        drawCircle(ctx, width - third / 2,
-            height * (1 - calcPercentage(values[1], values[2])),
-            dotWidth);
-    }, []);
+            const third = width / 3;
+
+            // Example drawing on the canvas
+            ctx.fillStyle = 'rgba(255, 255, 255, 0)';
+            ctx.fillRect(0, 0, width, height);
+            ctx.beginPath();
+
+            drawLine(ctx, third - third / 2,
+                height * (1 - calcPercentage(values[1], values[0])),
+                2 * third - third / 2,
+                height * 0.5);
+            drawLine(ctx, 2 * third - third / 2,
+                height * 0.5,
+                width - third / 2,
+                height * (1 - calcPercentage(values[1], values[2])));
+
+            drawCircle(ctx, third - third / 2,
+                height * (1 - calcPercentage(values[1], values[0])),
+                dotWidth);
+            drawCircle(ctx, 2 * third - third / 2,
+                height * 0.5,
+                dotWidth);
+            drawCircle(ctx, width - third / 2,
+                height * (1 - calcPercentage(values[1], values[2])),
+                dotWidth);
+        }
+    }, [dailyWidgetData]);
 
     return (<div className={`${className} bg-superClear rounded-xl shadow-md p-2 flex flex-col`}>
         <div className="flex flex-row h-max">
@@ -60,7 +75,7 @@ const DailyIncome = ({ className = "", inputValues = [0, 0, 0], dailyWidgetDate 
             <div className="grid grid-cols-3 grid-rows-1 h-full mt-5 text-center pb-6">
                 <span className="col-span-1 row-span-1 text-textSecoundary flex flex-col items-center justify-end text-sm pb-2 px-2">{sqlToFrenchDateDaily(formattedPreviousDates(dailyWidgetDate, 2))}</span>
                 <div className="col-span-1 row-span-1 flex flex-col justify-between items-center bg-darkPrimary p-2 rounded-xl text-white text-sm">
-                    <span className="font-medium">{formatNumberWithComma(values[1])} Ventes</span>
+                    <span className="font-medium">{formatNumberWithComma(result[1])} Ventes</span>
                     <span>{sqlToFrenchDateDaily(formattedPreviousDates(dailyWidgetDate, 1))}</span>
                 </div>
                 <span className="col-span-1 row-span-1 text-textSecoundary flex flex-col items-center justify-end text-sm pb-2 px-2">{sqlToFrenchDateDaily(dailyWidgetDate)}</span>
