08b34642e68d8d336d7f21f1d9281274b044dc3c
Mohamed Hichem Rahmani <hichem1029@live.fr>
2025-04-20 12:12:55 +0100

    feat(Dashboard): Add date selection functionality for graph widget

--- src/Views/dashboard.jsx
+++ src/Views/dashboard.jsx
@@ -22,9 +22,9 @@ const Dashboard = () => {
             {/* <DashHeader className="hidden sm:flex row-span-3 col-span-12 ml-4" /> */}
             <div className="row-span-5 col-span-12 flex-col space-y-4 small:space-y-0 small:flex small:flex-row small:justify-stretch small:space-x-4">
 
-                {!todayStats && (<div className={`w-full flex justify-center items-center`}>
+                {/* {!todayStats && (<div className={`w-full flex justify-center items-center`}>
                     <LoadingSpinner />
-                </div>)}
+                </div>)} */}
                 {/* {todayStats && (<DashCard className={`w-full`} post="Admin" name="You" adress="Boulevard des lions" imgSrc={pfp4} todaySales={todayStats && todayStats.total_sales} />)}
  */}
                 <LinearRadialBarWidget />
