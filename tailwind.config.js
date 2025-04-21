f7a911d888943f044c39633d654521936168609a
Mohamed Hichem Rahmani <hichem1029@live.fr>
2025-04-21 19:47:20 +0100

    feat(Auth): Switched from static css to dynamic Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import { Config } from "tailwindcss";
 
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
@@ -19,12 +20,33 @@ module.exports = {
         highlight: '#52D3FF',
         accent: '#F0A9F9'
       },
-    },
-    screens: {
-      'sm': '760px', //0
-      'small': '1180px', // 1
+      animation: {
+        "left-right": "animationLeftRight 2s ease-in-out infinite",
+        "right-left": "animationRightLeft 2s ease-in-out infinite",
+        "left-right-3s": "animationLeftRight 3s ease-in-out infinite",
+        "right-left-4s": "animationLeftRight 4s ease-in-out infinite",
+      },
+      screens: {
+        'sm': '760px', //0
+        'small': '1180px', // 1
+      },
+      keyframes: {
+        animationLeftRight: {
+          "0%": { transform: "translateX(0px)" },
+          "50%": { transform: "translateX(1000px)" },
+          "100%": { transform: "translateX(0px)" },
+        },
+        animationRightLeft: {
+          "0%": { transform: "translateX(0px)" },
+          "50%": { transform: "translateX(-1000px)" },
+          "100%": { transform: "translateX(0px)" },
+        },
+      },
     },
   },
   plugins: [],
+  future: {
+    disableColorOpacityUtilitiesByDefault: true, // Ensures CSS vars are generated
+  },
 }
 
